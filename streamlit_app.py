b170d7e81aa55e9d1c926b20eee9402eeaccb958
Hridijain18 <132593824+Hridijain18@users.noreply.github.com>
2023-05-04 16:47:38 +0100

    Update streamlit_app.py
    spelling mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu') 
 streamlit.text('Omega 3 and Blueberry Oatmeal') 
-streamlit.header('Kale, Spinach and Rocket Smoothie') 
-stramlit.header('Hard-Boiled Free-Range Egg') 
+streamlit.text('Kale, Spinach and Rocket Smoothie') 
+streamlit.text('Hard-Boiled Free-Range Egg') 
